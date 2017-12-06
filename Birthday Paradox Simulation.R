1f7f4f0f3df9b28252d868ce9278cbab5753cb4b
jsc034 <33695397+jsc034@users.noreply.github.com>
2017-12-06 12:58:18 -0800

    Add files via upload

---
+++ Birthday Paradox Simulation.R
@@ -0,0 +1,33 @@
+percentage <- 0
+runs <- 10000
+for(index in 1:runs){
+  room <- sample(1:365,23,replace=T)
+  #23 for 50% chance, 70 for 99.9% chance
+  #print(room)
+  for(i in 1:length(room)){
+    #print(i)
+    birthday1 <- room[i]
+    for(j in 1:length(room)){
+      if(j==i){
+       next;
+      }
+      #print(j)
+      found <- (room[i]==room[j])
+      #print(found)
+      if(found==TRUE){
+        break
+      }
+    }
+  if(found==TRUE){
+    break
+  }
+}
+# print(found)
+# print(room)
+if(found){
+  percentage <- percentage + 1
+}
+}
+percentage <- percentage/runs
+print(percentage)
+
